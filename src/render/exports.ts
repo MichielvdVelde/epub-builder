dbe9035c447497e25c31f70adf9c6349d2282ccc
Michiel van der Velde <michiel@michielvdvelde.nl>
2024-11-02 23:45:04 +0100

    fix: remove renderer exports

--- src/render/exports.ts
+++ src/render/exports.ts
@@ -3,6 +3,3 @@ export * from "./types";
 export * from "./errors";
 export * from "./helpers";
 export * from "./render";
-
-export * as ejs from "./renderers/ejs";
-export * as mustache from "./renderers/mustache";
